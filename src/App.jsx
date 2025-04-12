aaba93ea7c4ebc570a822d3824008d30887cd1a4
jabss <jabrijuhinin93@gmail.com>
2025-04-13 00:06:00 +0800

    revert back to default App.jsx code

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const counterReducer = (state, action) => {
-    if (action.type === 'INCREMENT') {
-      return state + 1
-    } else if (action.type === 'DECREMENT') {
-      return state - 1
-    } else if (action.type === 'ZERO') {
-      return 0
-    }
-  
-    return state
-  }
+  const [count, setCount] = useState(0)
 
-  // const [count, setCount] = useState(0)
-
-  // return (
-  //   <>
-  //     <div>
-  //       <a href="https://vite.dev" target="_blank">
-  //         <img src={viteLogo} className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.jsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //   </>
-  // )
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
